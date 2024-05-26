4816f22117ad26f73e94aff0b3a9b88bfb4e51ef
Maksym Andriiash <maksym.andriiash@kultrix.com>
2024-05-26 16:41:19 +0300

    [add] sound, email

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
 
+import emailjs from "@emailjs/browser";
+
 function App() {
   const [position, setPosition] = useState({ top: "40%", left: "40%" });
+
+  const yesSound = new Audio("/sexytime.ogg");
 
   const movePopup = () => {
     const newTop = Math.random() * 70;
@@ -10,16 +14,43 @@ function App() {
     setPosition({ top: `${newTop}%`, left: `${newLeft}%` });
   };
 
+  const handleYesClick = () => {
+    yesSound.play();
+    alert("Чекаю тебе в редрумі, маленька 😍🫣🤫😮‍💨😈💦");
+    sendEmail();
+  };
+
+  const templateParams = {
+    name: "Maksymka",
+    notes: "SHE SAID YES!",
+  };
+
+  const sendEmail = () => {
+    emailjs
+      .send(
+        process.env.REACT_APP_EMAIL_JS_SERVICE_ID,
+        process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID,
+        templateParams,
+        {
+          publicKey: process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY,
+        }
+      )
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        (err) => {
+          console.log("FAILED...", err);
+        }
+      );
+  };
+
   return (
     <div className="App">
       <div className="popup" style={{ top: position.top, left: position.left }}>
         <p>Вірт сьогодні ввечері?)))</p>
         <div className="btnWrapper">
-          <button
-            onClick={() => alert("Чекаю тебе в редрумі, маленька 😍🫣🤫😮‍💨😈💦")}
-          >
-            Yes!!!
-          </button>
+          <button onClick={handleYesClick}>Yes!!!</button>
           <button onMouseOver={movePopup} onTouchStart={movePopup}>
             No:(
           </button>
